862f29f6fc6035fd460f34a94711a77ddf443757
ivan <marciniwanczyk72@gmail.com>
2018-05-27 21:59:59 +0200

    Updates

--- script.js
+++ script.js
@@ -4,30 +4,5 @@ document.addEventListener("DOMContentLoaded", function () {
     const preloaderVar = document.getElementById("loader-wrapper");
     setTimeout(function () {
         preloaderVar.style.display = "none";
-
-        /*$(document).ready(function () {
-
-            let modal = $('.popupElement');
-            
-
-            modal.click(function () {
-                $(this).parent().next(modal).addClass('active');
-                $('body').addClass('hideBoddyScroll');
-            });
-
-            /*close.click(function () {
-
-                $(this).parent().parent(modal).removeClass('active');
-                $('body').removeClass('hideBoddyScroll');
-            });*/
-      /*  });**/
-
-
-      var popup = document.querySelector('.popupElement');
-
-      popup.addEventListener('click', function(){
-        popup.classList.add('active');
-      });
-
-    }, 5000);
+       }, 5000);
 });
